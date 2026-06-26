7873f924bf598b2bd035a9fe8d8d775c45f89c73
HP <HP@Venu>
2026-06-26 13:57:06 +0530

    Expliwait updated

--- selenium/src/test/java/waits/Expliwait.java
+++ selenium/src/test/java/waits/Expliwait.java
@@ -1,12 +1,21 @@
-//package waits;
-//
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//
-//public class Expliwait {
-//	public static void main(String[] args) {
-//		WebDriver driver=new ChromeDriver();
-//		driver.get(");
-//	}
-//
-//}
+package waits;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Expliwait {
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='twotabsearchtextbox']"))).sendKeys("Tshirts For Men");
+		
+	}
+
+}
